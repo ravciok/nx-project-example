bb3ff640012f14fb1c5c3594eb0bb05d0d0f7d20
ravciok <ravciok@gmail.com>
2025-01-09 15:07:53 +0100

    remove comments

--- libs/core-api/src/lib/loading.interceptor.ts
+++ libs/core-api/src/lib/loading.interceptor.ts
@@ -8,19 +8,14 @@ export const SkipLoading = new HttpContextToken<boolean>(() => false);
 export const loadingInterceptor: HttpInterceptorFn = (req, next) => {
   const loadingService = inject(LoaderService);
 
-  // Check for a custom attribute
-  // to avoid showing loading spinner
   if (req.context.get(SkipLoading)) {
-    // Pass the request directly to the next handler
     return next(req);
   }
 
-  // Turn on the loading spinner
   loadingService.changeState(true);
 
   return next(req).pipe(
     finalize(() => {
-      // Turn off the loading spinner
       loadingService.changeState(false);
     })
   );
